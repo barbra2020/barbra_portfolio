593168e537944fe3f49a8792a61eab8c509ab2e5
Barbra2020 <bmootian@gmail.com>
2021-01-27 14:49:35 +0300

    initial commit

---
+++ scully.barbra_portfolio.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "barbra_portfolio",
+  outDir: './dist/static',
+  routes: {
+  }
+};
